a4762b2dca53224aad33cdd296fe17820457c5bd
AlbertKovtoun <kovtoun.albert@hotmail.com>
2024-10-12 12:55:03 +0200

    :sparkles: Added time syncing

--- src/javascript/three/TimeModule.js
+++ src/javascript/three/TimeModule.js
@@ -1,11 +1,30 @@
 import { flights } from "./Experience"
+import { flightsArray } from "../data"
 
 export class TimeModule {
   constructor() {
-    this.time = document.querySelector(".time-module")
+    const waitForFlights = setInterval(() => {
+      if (flightsArray && flightsArray.length > 0) {
+        //.code here
+        clearInterval(waitForFlights)
+      }
+    }, 100)
 
-    console.log(this.time.textContent)
+    this.timeDisplay = document.querySelector(".time-module")
 
-    console.log(flights.flightProgresses)
+    this.progress = 0.6
+    this.syncTime(this.progress)
+  }
+
+  syncTime(progress) {
+    const totalMinutesInADay = 24 * 60
+    const currentMinutes = Math.round(progress * totalMinutesInADay)
+    const hours = Math.floor(currentMinutes / 60)
+    const minutes = currentMinutes % 60
+
+    const formattedHours = String(hours).padStart(2, "0")
+    const formattedMinutes = String(minutes).padStart(2, "0")
+
+    this.timeDisplay.textContent = `${formattedHours}:${formattedMinutes}`
   }
 }
