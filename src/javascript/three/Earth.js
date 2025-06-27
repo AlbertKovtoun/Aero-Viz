2f21a5b6e81a4b80e2740659defa372cf7a2d7c4
AlbertKovtoun <kovtoun.albert@hotmail.com>
2025-06-27 16:52:30 +0000

    Tweaked atmosphere

--- src/javascript/three/Earth.js
+++ src/javascript/three/Earth.js
@@ -19,6 +19,7 @@ import {
   cameraPosition,
   positionWorld,
   add,
+  min,
   abs,
 } from "three/tsl"
 import { loaders, renderer, scene } from "./Experience"
@@ -27,6 +28,7 @@ export class Earth {
   constructor() {
     this.setTextures()
     this.setEarth()
+    this.setAtmosphere()
     this.setClouds()
   }
 
@@ -47,6 +49,7 @@ export class Earth {
       "/textures/earth-roughness.jpg",
     )
     this.earthRoughnessTexture.colorSpace = THREE.SRGBColorSpace
+    this.earthRoughnessTexture.anisotropy = 8
 
     this.cloudsTexture = loaders.textureLoader.load(
       "/textures/earth-clouds.jpg",
@@ -55,7 +58,7 @@ export class Earth {
     this.cloudsTexture.anisotropy = 8
   }
 
-  setEarthMaterial() {
+  setEarth() {
     this.earthMaterial = new THREE.MeshBasicNodeMaterial()
 
     const viewDirection = normalize(cameraPosition.sub(positionWorld))
@@ -81,37 +84,76 @@ export class Earth {
     let phongValue = max(0.0, dot(viewDirection, reflectedLight))
     phongValue = pow(phongValue, 128.0)
 
-    const specularColor = vec3(1.0, 1.0, 1.0)
-    let specular = specularColor.mul(phongValue.mul(roughness))
-
     //Fresnel
     let fresnel = oneMinus(max(0.0, dot(viewDirection, normalWorld)))
-    fresnel = pow(fresnel, 4.0)
+    fresnel = pow(fresnel, 2.0)
 
     const atmosphereColor = vec3(0.4, 0.7, 1.0)
-    const sunsetColor = vec3(1.0, 0.6, 0.2)
+    const sunsetColor = vec3(1.0, 0.2, 0.2)
 
-    const terminatorFactor = oneMinus(smoothstep(0.0, 0.2, abs(sunLight)))
+    const terminatorFactor = oneMinus(smoothstep(0.0, 0.8, abs(sunLight)))
     const dynamicGlowColor = mix(atmosphereColor, sunsetColor, terminatorFactor)
 
     const nightOcclusion = smoothstep(-0.1, 0.1, sunLight)
 
-    const fresnelEffect = dynamicGlowColor.mul(fresnel).mul(nightOcclusion)
+    fresnel = fresnel.mul(dynamicGlowColor).mul(nightOcclusion)
 
-    const finalColor = color.rgb.add(specular).add(fresnelEffect)
-    // const finalColor = vec3(nightOcclusion)
+    let specularColor = mix(vec3(1.0), dynamicGlowColor, fresnel.mul(2))
+    let specular = specularColor.mul(phongValue.mul(roughness))
+
+    const finalColor = color.rgb.add(specular).add(fresnel)
+    // const finalColor = vec3(specularColor)
 
     this.earthMaterial.colorNode = finalColor
 
-    return this.earthMaterial
-  }
-
-  setEarth() {
     this.earth = new THREE.Mesh(
       new THREE.SphereGeometry(1, 64, 64),
-      this.setEarthMaterial(),
+      this.earthMaterial,
     )
     scene.add(this.earth)
+  }
+
+  setAtmosphere() {
+    this.atmosphereMaterial = new THREE.MeshBasicNodeMaterial({
+      transparent: true,
+      side: THREE.BackSide,
+      blending: THREE.AdditiveBlending,
+      depthWrite: false,
+    })
+
+    const viewDirection = normalize(cameraPosition.sub(positionWorld))
+
+    // Lighting
+    const sunDirection = normalize(vec3(1.0, 0.2, 0.0))
+    const sunLight = dot(sunDirection, normalWorld)
+
+    //Fresnel
+    let fresnel = oneMinus(max(0.0, dot(viewDirection, normalWorld)))
+    fresnel = pow(fresnel, 2.0)
+
+    let fadeOut = fresnel
+
+    const atmosphereColor = vec3(0.4, 0.7, 1.0)
+    const sunsetColor = vec3(1.0, 0.2, 0.2)
+
+    const terminatorFactor = oneMinus(smoothstep(0.0, 0.8, abs(sunLight)))
+    const dynamicGlowColor = mix(atmosphereColor, sunsetColor, terminatorFactor)
+
+    const nightOcclusion = smoothstep(-0.1, 0.1, sunLight)
+
+    fresnel = fresnel.mul(dynamicGlowColor).mul(nightOcclusion)
+
+    const finalColor = fresnel
+
+    this.atmosphereMaterial.colorNode = fresnel
+
+    this.atmosphereMaterial.opacityNode = nightOcclusion
+
+    this.atmosphere = new THREE.Mesh(
+      new THREE.SphereGeometry(1.02, 64, 64),
+      this.atmosphereMaterial,
+    )
+    // scene.add(this.atmosphere)
   }
 
   setClouds() {
@@ -127,14 +169,14 @@ export class Earth {
     )
 
     this.clouds = new THREE.Mesh(
-      new THREE.SphereGeometry(1.01, 64, 64),
+      new THREE.SphereGeometry(1.001, 64, 64),
       this.cloudsMaterial,
     )
-    scene.add(this.clouds)
+    // scene.add(this.clouds)
   }
 
   update(deltaTime) {
-    this.earth.rotateY(deltaTime * 0.0001)
-    this.clouds.rotateY(deltaTime * 0.0001)
+    // this.earth.rotateY(deltaTime * 0.0001)
+    // this.clouds.rotateY(deltaTime * 0.0001)
   }
 }
